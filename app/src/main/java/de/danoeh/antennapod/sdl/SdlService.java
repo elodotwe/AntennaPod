120ead7d50fa5632e91143c5e790709209d07e1a
Jacob Rau <jarau@mtu.edu>
2018-07-17 22:10:51 -0400

    Add logging, subscribe to transport buttons on startup

--- app/src/main/java/de/danoeh/antennapod/sdl/SdlService.java
+++ app/src/main/java/de/danoeh/antennapod/sdl/SdlService.java
@@ -12,6 +12,7 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.smartdevicelink.exception.SdlException;
+import com.smartdevicelink.proxy.RPCStruct;
 import com.smartdevicelink.proxy.SdlProxyALM;
 import com.smartdevicelink.proxy.callbacks.OnServiceEnded;
 import com.smartdevicelink.proxy.callbacks.OnServiceNACKed;
@@ -73,6 +74,7 @@ import com.smartdevicelink.proxy.rpc.ShowResponse;
 import com.smartdevicelink.proxy.rpc.SliderResponse;
 import com.smartdevicelink.proxy.rpc.SpeakResponse;
 import com.smartdevicelink.proxy.rpc.StreamRPCResponse;
+import com.smartdevicelink.proxy.rpc.SubscribeButton;
 import com.smartdevicelink.proxy.rpc.SubscribeButtonResponse;
 import com.smartdevicelink.proxy.rpc.SubscribeVehicleDataResponse;
 import com.smartdevicelink.proxy.rpc.SubscribeWayPointsResponse;
@@ -81,8 +83,12 @@ import com.smartdevicelink.proxy.rpc.UnsubscribeButtonResponse;
 import com.smartdevicelink.proxy.rpc.UnsubscribeVehicleDataResponse;
 import com.smartdevicelink.proxy.rpc.UnsubscribeWayPointsResponse;
 import com.smartdevicelink.proxy.rpc.UpdateTurnListResponse;
+import com.smartdevicelink.proxy.rpc.enums.ButtonName;
+import com.smartdevicelink.proxy.rpc.enums.HMILevel;
 import com.smartdevicelink.proxy.rpc.enums.SdlDisconnectedReason;
 import com.smartdevicelink.transport.TransportConstants;
+
+import org.json.JSONException;
 
 public class SdlService extends Service implements IProxyListenerALM {
     private final static int NOTIFICATION_ID = 1; // Chosen randomly by rolling a 6 sided die
@@ -91,6 +97,16 @@ public class SdlService extends Service implements IProxyListenerALM {
 
     //The proxy handles communication between the application and SDL
     private SdlProxyALM proxy = null;
+
+    private boolean isFirstRun = true;
+
+    private String serializeOrFart(RPCStruct struct) {
+        try {
+            return struct.serializeJSON().toString(2);
+        } catch (JSONException e) {
+            return "Could not serialize struct.";
+        }
+    }
 
     @Override
     public void onDestroy() {
@@ -143,9 +159,29 @@ public class SdlService extends Service implements IProxyListenerALM {
         return null;
     }
 
+    private void subscribeButton(ButtonName buttonName) {
+        SubscribeButton subscribeButton = new SubscribeButton();
+        subscribeButton.setButtonName(buttonName);
+        try {
+            proxy.sendRPCRequest(subscribeButton);
+        } catch (SdlException e) {
+            Log.e(TAG, "onOnHMIStatus: Unable to subscribe to button " + buttonName.name(), e);
+        }
+    }
+
     @Override
     public void onOnHMIStatus(OnHMIStatus notification) {
+        Log.d(TAG, "onOnHMIStatus() called with: notification = [" + serializeOrFart(notification) + "]");
 
+        if (notification.getHmiLevel() == HMILevel.HMI_FULL) {
+            if (isFirstRun) {
+                subscribeButton(ButtonName.OK);
+                subscribeButton(ButtonName.SEEKLEFT);
+                subscribeButton(ButtonName.SEEKRIGHT);
+
+                isFirstRun = false;
+            }
+        }
     }
 
     @Override
@@ -156,272 +192,272 @@ public class SdlService extends Service implements IProxyListenerALM {
 
     @Override
     public void onServiceEnded(OnServiceEnded serviceEnded) {
-
+        Log.d(TAG, "onServiceEnded() called with: serviceEnded = [" + serviceEnded + "]");
     }
 
     @Override
     public void onServiceNACKed(OnServiceNACKed serviceNACKed) {
-
+        Log.d(TAG, "onServiceNACKed() called with: serviceNACKed = [" + serviceNACKed + "]");
     }
 
     @Override
     public void onOnStreamRPC(OnStreamRPC notification) {
-
+        Log.d(TAG, "onOnStreamRPC() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onStreamRPCResponse(StreamRPCResponse response) {
-
+        Log.d(TAG, "onStreamRPCResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onError(String info, Exception e) {
-
+        Log.e(TAG, "onError: info=" + info, e);
     }
 
     @Override
     public void onGenericResponse(GenericResponse response) {
-
+        Log.d(TAG, "onGenericResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onOnCommand(OnCommand notification) {
-
+        Log.d(TAG, "onOnCommand() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onAddCommandResponse(AddCommandResponse response) {
-
+        Log.d(TAG, "onAddCommandResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onAddSubMenuResponse(AddSubMenuResponse response) {
-
+        Log.d(TAG, "onAddSubMenuResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onCreateInteractionChoiceSetResponse(CreateInteractionChoiceSetResponse response) {
-
+        Log.d(TAG, "onCreateInteractionChoiceSetResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onAlertResponse(AlertResponse response) {
-
+        Log.d(TAG, "onAlertResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onDeleteCommandResponse(DeleteCommandResponse response) {
-
+        Log.d(TAG, "onDeleteCommandResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onDeleteInteractionChoiceSetResponse(DeleteInteractionChoiceSetResponse response) {
-
+        Log.d(TAG, "onDeleteInteractionChoiceSetResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onDeleteSubMenuResponse(DeleteSubMenuResponse response) {
-
+        Log.d(TAG, "onDeleteSubMenuResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onPerformInteractionResponse(PerformInteractionResponse response) {
-
+        Log.d(TAG, "onPerformInteractionResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onResetGlobalPropertiesResponse(ResetGlobalPropertiesResponse response) {
-
+        Log.d(TAG, "onResetGlobalPropertiesResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onSetGlobalPropertiesResponse(SetGlobalPropertiesResponse response) {
-
+        Log.d(TAG, "onSetGlobalPropertiesResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onSetMediaClockTimerResponse(SetMediaClockTimerResponse response) {
-
+        Log.d(TAG, "onSetMediaClockTimerResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onShowResponse(ShowResponse response) {
-
+        Log.d(TAG, "onShowResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onSpeakResponse(SpeakResponse response) {
-
+        Log.d(TAG, "onSpeakResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onOnButtonEvent(OnButtonEvent notification) {
-
+        Log.d(TAG, "onOnButtonEvent() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onOnButtonPress(OnButtonPress notification) {
-
+        Log.d(TAG, "onOnButtonPress() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onSubscribeButtonResponse(SubscribeButtonResponse response) {
-
+        Log.d(TAG, "onSubscribeButtonResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onUnsubscribeButtonResponse(UnsubscribeButtonResponse response) {
-
+        Log.d(TAG, "onUnsubscribeButtonResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onOnPermissionsChange(OnPermissionsChange notification) {
-
+        Log.d(TAG, "onOnPermissionsChange() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onSubscribeVehicleDataResponse(SubscribeVehicleDataResponse response) {
-
+        Log.d(TAG, "onSubscribeVehicleDataResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onUnsubscribeVehicleDataResponse(UnsubscribeVehicleDataResponse response) {
-
+        Log.d(TAG, "onUnsubscribeVehicleDataResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onGetVehicleDataResponse(GetVehicleDataResponse response) {
-
+        Log.d(TAG, "onGetVehicleDataResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onOnVehicleData(OnVehicleData notification) {
-
+        Log.d(TAG, "onOnVehicleData() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onPerformAudioPassThruResponse(PerformAudioPassThruResponse response) {
-
+        Log.d(TAG, "onPerformAudioPassThruResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onEndAudioPassThruResponse(EndAudioPassThruResponse response) {
-
+        Log.d(TAG, "onEndAudioPassThruResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onOnAudioPassThru(OnAudioPassThru notification) {
-
+        Log.d(TAG, "onOnAudioPassThru() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onPutFileResponse(PutFileResponse response) {
-
+        Log.d(TAG, "onPutFileResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onDeleteFileResponse(DeleteFileResponse response) {
-
+        Log.d(TAG, "onDeleteFileResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onListFilesResponse(ListFilesResponse response) {
-
+        Log.d(TAG, "onListFilesResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onSetAppIconResponse(SetAppIconResponse response) {
-
+        Log.d(TAG, "onSetAppIconResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onScrollableMessageResponse(ScrollableMessageResponse response) {
-
+        Log.d(TAG, "onScrollableMessageResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onChangeRegistrationResponse(ChangeRegistrationResponse response) {
-
+        Log.d(TAG, "onChangeRegistrationResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onSetDisplayLayoutResponse(SetDisplayLayoutResponse response) {
-
+        Log.d(TAG, "onSetDisplayLayoutResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onOnLanguageChange(OnLanguageChange notification) {
-
+        Log.d(TAG, "onOnLanguageChange() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onOnHashChange(OnHashChange notification) {
-
+        Log.d(TAG, "onOnHashChange() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onSliderResponse(SliderResponse response) {
-
+        Log.d(TAG, "onSliderResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onOnDriverDistraction(OnDriverDistraction notification) {
-
+        Log.d(TAG, "onOnDriverDistraction() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onOnTBTClientState(OnTBTClientState notification) {
-
+        Log.d(TAG, "onOnTBTClientState() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onOnSystemRequest(OnSystemRequest notification) {
-
+        Log.d(TAG, "onOnSystemRequest() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onSystemRequestResponse(SystemRequestResponse response) {
-
+        Log.d(TAG, "onSystemRequestResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onOnKeyboardInput(OnKeyboardInput notification) {
-
+        Log.d(TAG, "onOnKeyboardInput() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onOnTouchEvent(OnTouchEvent notification) {
-
+        Log.d(TAG, "onOnTouchEvent() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onDiagnosticMessageResponse(DiagnosticMessageResponse response) {
-
+        Log.d(TAG, "onDiagnosticMessageResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onReadDIDResponse(ReadDIDResponse response) {
-
+        Log.d(TAG, "onReadDIDResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onGetDTCsResponse(GetDTCsResponse response) {
-
+        Log.d(TAG, "onGetDTCsResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onOnLockScreenNotification(OnLockScreenStatus notification) {
-
+        Log.d(TAG, "onOnLockScreenNotification() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onDialNumberResponse(DialNumberResponse response) {
-
+        Log.d(TAG, "onDialNumberResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onSendLocationResponse(SendLocationResponse response) {
-
+        Log.d(TAG, "onSendLocationResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
@@ -451,17 +487,17 @@ public class SdlService extends Service implements IProxyListenerALM {
 
     @Override
     public void onShowConstantTbtResponse(ShowConstantTbtResponse response) {
-
+        Log.d(TAG, "onShowConstantTbtResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onAlertManeuverResponse(AlertManeuverResponse response) {
-
+        Log.d(TAG, "onAlertManeuverResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onUpdateTurnListResponse(UpdateTurnListResponse response) {
-
+        Log.d(TAG, "onUpdateTurnListResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
@@ -471,51 +507,51 @@ public class SdlService extends Service implements IProxyListenerALM {
 
     @Override
     public void onGetWayPointsResponse(GetWayPointsResponse response) {
-
+        Log.d(TAG, "onGetWayPointsResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onSubscribeWayPointsResponse(SubscribeWayPointsResponse response) {
-
+        Log.d(TAG, "onSubscribeWayPointsResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onUnsubscribeWayPointsResponse(UnsubscribeWayPointsResponse response) {
-
+        Log.d(TAG, "onUnsubscribeWayPointsResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onOnWayPointChange(OnWayPointChange notification) {
-
+        Log.d(TAG, "onOnWayPointChange() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onGetSystemCapabilityResponse(GetSystemCapabilityResponse response) {
-
+        Log.d(TAG, "onGetSystemCapabilityResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onGetInteriorVehicleDataResponse(GetInteriorVehicleDataResponse response) {
-
+        Log.d(TAG, "onGetInteriorVehicleDataResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onButtonPressResponse(ButtonPressResponse response) {
-
+        Log.d(TAG, "onButtonPressResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onSetInteriorVehicleDataResponse(SetInteriorVehicleDataResponse response) {
-
+        Log.d(TAG, "onSetInteriorVehicleDataResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 
     @Override
     public void onOnInteriorVehicleData(OnInteriorVehicleData notification) {
-
+        Log.d(TAG, "onOnInteriorVehicleData() called with: notification = [" + serializeOrFart(notification) + "]");
     }
 
     @Override
     public void onSendHapticDataResponse(SendHapticDataResponse response) {
-
+        Log.d(TAG, "onSendHapticDataResponse() called with: response = [" + serializeOrFart(response) + "]");
     }
 }
