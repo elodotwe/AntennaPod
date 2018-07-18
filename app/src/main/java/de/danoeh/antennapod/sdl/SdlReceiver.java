dbc3ebf02612d0f7303c0d5104992a0995675624
Jacob Rau <jarau@mtu.edu>
2018-07-17 21:22:37 -0400

    Add a bit of logging

--- app/src/main/java/de/danoeh/antennapod/sdl/SdlReceiver.java
+++ app/src/main/java/de/danoeh/antennapod/sdl/SdlReceiver.java
@@ -3,11 +3,14 @@ package de.danoeh.antennapod.sdl;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.util.Log;
 
 import com.smartdevicelink.transport.SdlBroadcastReceiver;
 import com.smartdevicelink.transport.SdlRouterService;
 
 public class SdlReceiver extends SdlBroadcastReceiver {
+    private static final String TAG = "SdlReceiver";
+
     @Override
     public Class<? extends SdlRouterService> defineLocalSdlRouterClass() {
         return de.danoeh.antennapod.sdl.SdlRouterService.class;
@@ -15,6 +18,7 @@ public class SdlReceiver extends SdlBroadcastReceiver {
 
     @Override
     public void onSdlEnabled(Context context, Intent intent) {
+        Log.i(TAG, "onSdlEnabled; spinning up service");
         //Use the provided intent but set the class to the SdlService
         intent.setClass(context, SdlService.class);
         if(Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
